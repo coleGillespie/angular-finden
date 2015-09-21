560d9f5d28e1ed78117f1a039515843c29b069ba
coleGillespie <mcg42387@gmail.com>
2015-09-21 15:38:12 -0400

    Added google maps, now mapping tweets.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
 var socket = io.connect('http://localhost:3030');
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
 socket.on('mapTweet', function (data) {
   console.log(data.text);
   console.log(data.geo.coordinates);
+  var marker = new google.maps.Marker({
+      position:  {
+        lat: data.geo.coordinates[0],
+        lng: data.geo.coordinates[1]
+      },
+      map: map
+  });
 });
